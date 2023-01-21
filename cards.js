cd5037a30b4d84bf7b1d6861694e3a93d4ce740d
Liza Krestyaninova <liza0410200@gmail.com>
2023-01-21 21:20:28 +0300

    hw3 fix

---
+++ cards.js
@@ -0,0 +1,229 @@
+const cardArr = [
+    {
+        suit: 'peaks',
+        rang: '6',
+        src: 'src/img/6 пики.svg',
+    },
+
+    {
+        suit: 'peaks',
+        rang: '7',
+        src: 'src/img/7 пики.svg',
+    },
+
+    {
+        suit: 'peaks',
+        rang: '8',
+        src: 'src/img/8 пики.svg',
+    },
+
+    {
+        suit: 'peaks',
+        rang: '9',
+        src: 'src/img/9 пики.svg',
+    },
+
+    {
+        suit: 'peaks',
+        rang: '10',
+        src: 'src/img/10 пики.svg',
+    },
+
+    {
+        suit: 'peaks',
+        rang: 'j',
+        src: 'src/img/валет пики.svg',
+    },
+
+    {
+        suit: 'peaks',
+        rang: 'q',
+        src: 'src/img/дама пики.svg',
+    },
+
+    {
+        suit: 'peaks',
+        rang: 'k',
+        src: 'src/img/король пики.svg',
+    },
+
+    {
+        suit: 'peaks',
+        rang: 'a',
+        src: 'src/img/туз пики.svg',
+    },
+
+    {
+        suit: 'hearts',
+        rang: '6',
+        src: 'src/img/6 черви.svg',
+    },
+
+    {
+        suit: 'hearts',
+        rang: '7',
+        src: 'src/img/7 черви.svg',
+    },
+
+    {
+        suit: 'hearts',
+        rang: '8',
+        src: 'src/img/8 черви.svg',
+    },
+
+    {
+        suit: 'hearts',
+        rang: '9',
+        src: 'src/img/9 черви.svg',
+    },
+
+    {
+        suit: 'hearts',
+        rang: '10',
+        src: 'src/img/10 черви.svg',
+    },
+
+    {
+        suit: 'hearts',
+        rang: 'j',
+        src: 'src/img/валет черви.svg',
+    },
+
+    {
+        suit: 'hearts',
+        rang: 'q',
+        src: 'src/img/дама черви.svg',
+    },
+
+    {
+        suit: 'hearts',
+        rang: 'k',
+        src: 'src/img/король черви.svg',
+    },
+
+    {
+        suit: 'hearts',
+        rang: 'a',
+        src: 'src/img/туз черви.svg',
+    },
+
+    {
+        suit: 'diamonds',
+        rang: '6',
+        src: 'src/img/6 бубны.svg',
+    },
+
+    {
+        suit: 'diamonds',
+        rang: '7',
+        src: 'src/img/7 бубны.svg',
+    },
+
+    {
+        suit: 'diamonds',
+        rang: '8',
+        src: 'src/img/8 бубны.svg',
+    },
+
+    {
+        suit: 'diamonds',
+        rang: '9',
+        src: 'src/img/9 бубны.svg',
+    },
+
+    {
+        suit: 'diamonds',
+        rang: '10',
+        src: 'src/img/10 бубны.svg',
+    },
+
+    {
+        suit: 'diamonds',
+        rang: 'j',
+        src: 'src/img/валет бубны.svg',
+    },
+
+    {
+        suit: 'diamonds',
+        rang: 'q',
+        src: 'src/img/дама бубны.svg',
+    },
+
+    {
+        suit: 'diamonds',
+        rang: 'k',
+        src: 'src/img/король бубны.svg',
+    },
+
+    {
+        suit: 'diamonds',
+        rang: 'a',
+        src: 'src/img/туз бубны.svg',
+    },
+
+    {
+        suit: 'diamonds',
+        rang: '6',
+        src: 'src/img/6 бубны.svg',
+    },
+
+    {
+        suit: 'clubs',
+        rang: '6',
+        src: 'src/img/6 крести.svg',
+    },
+
+    {
+        suit: 'clubs',
+        rang: '7',
+        src: 'src/img/7 крести.svg',
+    },
+
+    {
+        suit: 'clubs',
+        rang: '8',
+        src: 'src/img/8 крести.svg',
+    },
+
+    {
+        suit: 'clubs',
+        rang: '9',
+        src: 'src/img/9 крести.svg',
+    },
+
+    {
+        suit: 'clubs',
+        rang: '9',
+        src: 'src/img/9 крести.svg',
+    },
+
+    {
+        suit: 'clubs',
+        rang: '10',
+        src: 'src/img/10 крести.svg',
+    },
+
+    {
+        suit: 'clubs',
+        rang: 'j',
+        src: 'src/img/валет крести.svg',
+    },
+
+    {
+        suit: 'clubs',
+        rang: 'q',
+        src: 'src/img/дама крести.svg',
+    },
+
+    {
+        suit: 'clubs',
+        rang: 'k',
+        src: 'src/img/король крести.svg',
+    },
+
+    {
+        suit: 'clubs',
+        rang: 'a',
+        src: 'src/img/туз крести.svg',
+    },
+]
