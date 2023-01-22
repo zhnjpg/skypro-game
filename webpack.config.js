83967c2781a7b7170755e92e24338a6aad4f7ad4
vivakhnyuk <topkekhs1337@gmail.com>
2023-01-23 01:21:48 +0300

    hw4 commit2

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,28 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+
 module.exports = {
-    entry: './index.js',
+  entry: './game.js',
+  mode,
+  module: {
+    rules: [
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      }
+    ]
+  },
+  devtool: process.env.NODE_ENV === "production" ? "hidden-source-map" : "source-map",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [new HtmlWebpackPlugin()],
 }
